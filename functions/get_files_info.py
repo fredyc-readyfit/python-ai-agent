0790f34de88041228a34472f036dea55c163071e
Fredy Augusto Castellon Bedoya <fredyc@readyfit.com>
2025-06-23 10:49:09 -0600

    CH2: Functions - Get Files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,25 +1,27 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    working_directory = os.path.abspath(working_directory)
-    directory = os.path.abspath(os.path.join(working_directory, directory))
+    abs_working_directory = os.path.abspath(working_directory)
+    target_dir = abs_working_directory
+    if directory:
+        target_dir = os.path.abspath(os.path.join(working_directory, directory))
 
-    if not f"{directory}".startswith(working_directory):
+    if not f"{target_dir}".startswith(abs_working_directory):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    if not os.path.isdir(directory):
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
     
     try:
         files_info = []
-        dir_contents = os.listdir(directory)
+        dir_contents = os.listdir(target_dir)
         for c in dir_contents:
-            c_path = os.path.join(directory, c)
+            c_path = os.path.join(target_dir, c)
             is_dir = os.path.isdir(c_path)
             # is_file = os.path.isfile(c_path)
             file_size = os.path.getsize(c_path)
-            files_info.append(f"- {c}: file_size={file_size} bytes, is_dir={is_dir}\n")
+            files_info.append(f"- {c}: file_size={file_size} bytes, is_dir={is_dir}")
             
-        return "".join(files_info)
-    except:
-        return f"Error: Unexpected error"
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error: exception listing files {e}"
--- tests.py
+++ tests.py
@@ -1,9 +1,24 @@
 from functions.get_files_info import get_files_info
 
-print(get_files_info("calculator", "."))
 
-print(get_files_info("calculator", "pkg"))
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
 
-print(get_files_info("calculator", "/bin"))
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
 
-print(get_files_info("calculator", "../"))
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
