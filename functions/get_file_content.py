69f14ef09cb4d7b4d66e25c8db7fe0d3b85f6338
Fredy Augusto Castellon Bedoya <fredyc@readyfit.com>
2025-06-24 21:04:08 -0500

    CH2: Functions - Get File Content

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not file_path.startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        
+        if len(file_content_string) == MAX_CHARS:
+            file_content_string += f'\n[...File "{file_path}" truncated at 10000 characters]'
+        
+        return file_content_string
+    except Exception as e:
+        return f"Erro: exception reading file {e}"
